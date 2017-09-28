a7315efa059b314d82f80bc66f31ede1ff4f7e93
Frank McCown <fmccown@harding.edu>
2017-09-28 10:42:00 -0500

    Added version 3

---
+++ TheBandDatabaseV3/app/src/main/java/com/zybooks/thebanddatabase/ListActivity.java
@@ -0,0 +1,34 @@
+package com.zybooks.thebanddatabase;
+
+import android.content.Intent;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ListActivity extends AppCompatActivity implements ListFragment.OnBandSelectedListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        Fragment fragment = fragmentManager.findFragmentById(R.id.list_fragment_container);
+
+        if (fragment == null) {
+            fragment = new ListFragment();
+            fragmentManager.beginTransaction()
+                    .add(R.id.list_fragment_container, fragment)
+                    .commit();
+        }
+    }
+
+    @Override
+    public void onBandSelected(int bandId) {
+        // Send the band ID of the clicked button to DetailsActivity
+        Intent intent = new Intent(this, DetailsActivity.class);
+        intent.putExtra(DetailsActivity.EXTRA_BAND_ID, bandId);
+        startActivity(intent);
+    }
+}
---
+++ TheBandDatabaseV3/app/src/main/java/com/zybooks/thebanddatabase/ListFragment.java
@@ -0,0 +1,119 @@
+package com.zybooks.thebanddatabase;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class ListFragment extends Fragment {
+
+    // For the activity to implement
+    public interface OnBandSelectedListener {
+        void onBandSelected(int bandId);
+    }
+
+    // Reference to the activity
+    private OnBandSelectedListener mListener;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_list, container, false);
+
+        RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.band_recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        // Send bands to recycler view
+        BandAdapter adapter = new BandAdapter(BandDatabase.get(getContext()).getBands());
+        recyclerView.setAdapter(adapter);
+
+        return view;
+    }
+
+    private class BandHolder extends RecyclerView.ViewHolder
+            implements View.OnClickListener {
+
+        private Band mBand;
+
+        private TextView mNameTextView;
+
+        public BandHolder(LayoutInflater inflater, ViewGroup parent) {
+            super(inflater.inflate(R.layout.list_item_band, parent, false));
+            itemView.setOnClickListener(this);
+            mNameTextView = (TextView) itemView.findViewById(R.id.bandName);
+        }
+
+        public void bind(Band band) {
+            mBand = band;
+            mNameTextView.setText(mBand.getName());
+        }
+
+        @Override
+        public void onClick(View view) {
+            // Tell ListActivity what band was clicked
+            mListener.onBandSelected(mBand.getId());
+        }
+    }
+
+    private class BandAdapter extends RecyclerView.Adapter<BandHolder> {
+
+        private List<Band> mBands;
+
+        public BandAdapter(List<Band> bands) {
+            mBands = bands;
+        }
+
+        @Override
+        public BandHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            LayoutInflater layoutInflater = LayoutInflater.from(getActivity());
+            return new BandHolder(layoutInflater, parent);
+        }
+
+        @Override
+        public void onBindViewHolder(BandHolder holder, int position) {
+            Band band = mBands.get(position);
+            holder.bind(band);
+        }
+
+        @Override
+        public int getItemCount() {
+            return mBands.size();
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnBandSelectedListener) {
+            mListener = (OnBandSelectedListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnBandSelectedListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    private View.OnClickListener buttonClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            // Notify activity of band selection
+            String bandId = (String) view.getTag();
+            mListener.onBandSelected(Integer.parseInt(bandId));
+        }
+    };
+}
